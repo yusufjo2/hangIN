769321fb3953044f0536f3fe1a9af23c14842452
yusufjo2 <yusuf0123alshafiy@gmail.com>
2020-03-04 20:04:22 +0200

    init commit

---
+++ app/src/main/java/yousef/com/hangin/Splash_Activity.java
@@ -0,0 +1,34 @@
+package yousef.com.hangin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                navigation();
+
+            }
+        },3000);
+    }
+
+    void navigation(){
+        Intent intent = new Intent(this, SignInActivity.class);
+        startActivity(intent);
+    }
+}
